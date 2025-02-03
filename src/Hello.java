403affeeafd79b1616e3ea8946e2ffb287c61611
Ismael Velazquez <128411935+Ivela183@users.noreply.github.com>
2025-02-03 00:34:27 +0000

    First Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,7 @@
 // Hello World
+public class Hello{
+    public static void main(String[] arg){
+        //prints out string in java using System.out.println
+        System.out.println("Hello World");
+    }
+}
